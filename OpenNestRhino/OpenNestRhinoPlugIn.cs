7064ed8ac6c951554d44620a6f67c61f261d20a8
Petras Vestartas <petrasvestartas@gmail.com>
2019-10-04 11:41:42 +0300

    OpenNest1_

---
+++ OpenNestRhino/OpenNestRhinoPlugIn.cs
@@ -0,0 +1,24 @@
+﻿namespace OpenNestRhino {
+    ///<summary>
+    /// <para>Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+    /// class. DO NOT create instances of this class yourself. It is the
+    /// responsibility of Rhino to create an instance of this class.</para>
+    /// <para>To complete plug-in information, please also see all PlugInDescription
+    /// attributes in AssemblyInfo.cs (you might need to click "Project" ->
+    /// "Show All Files" to see it in the "Solution Explorer" window).</para>
+    ///</summary>
+    public class OpenNestRhinoPlugIn : Rhino.PlugIns.PlugIn {
+        public OpenNestRhinoPlugIn() {
+            Instance = this;
+        }
+
+        ///<summary>Gets the only instance of the OpenNestRhinoPlugIn plug-in.</summary>
+        public static OpenNestRhinoPlugIn Instance {
+            get; private set;
+        }
+
+        // You can override methods here to change the plug-in behavior on
+        // loading and shut down, add options pages to the Rhino _Option command
+        // and maintain plug-in wide options in a document.
+    }
+}
